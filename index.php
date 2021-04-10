9bd82ab670c64831665bd106593970c5a3de7fd9
ChillySpiral <81975828+ChillySpiral@users.noreply.github.com>
2021-04-10 23:16:32 +0200

    Erste CSS Datei aus Bootstrap Studio plus gestaltung der Index Datei

--- index.php
+++ index.php
@@ -1,12 +1,34 @@
 <!DOCTYPE html>
-<html lang="de">
+<html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Studyshare</title>
+    <link rel="stylesheet" href="CSS/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="CSS/Footer-Basic.css">
+    <link rel="stylesheet" href="CSS/styles.css">
 </head>
-<body>
-    
+
+<body style="background: #f7f7ff;">
+    <nav class="navbar navbar-light navbar-expand-md sticky-top" id="my-navbar" style="background: #577399;border-radius: 0;border-bottom-right-radius: 7px;border-bottom-left-radius: 7px;">
+        <div class="container-fluid"><a class="navbar-brand" id="header-links" href="#" style="font-size: 30px;">StudyShare</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse justify-content-end" id="navcol-1" style="color: rgb(255,255,255);">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link active" id="header-links" href="#" style="padding: 8px;">Anmelden</a></li>
+                    <li class="nav-item"><a class="nav-link d-inline-flex" id="header-links" href="#" style="background: #fe5f55;border-radius: 16px;padding: 8px;">Registrieren</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <footer class="d-flex justify-content-end footer-basic" id="my-footer">
+        <ul class="list-inline" style="background: #fe5f55;margin-top: 22px;margin-right: 30px;padding-right: 10px;padding-left: 10px;border-top-left-radius: 35px;border-top-right-radius: 35px;font-size: 24px;color: rgb(255,255,255);">
+            <li class="list-inline-item"><a href="#">Impressum</a></li>
+            <li class="list-inline-item"><a href="#">FAQ</a></li>
+        </ul>
+    </footer>
+    <script src="Javascript - Typescript/js/jquery.min.js"></script>
+    <script src="CSS/bootstrap/js/bootstrap.min.js"></script>
 </body>
+
 </html>
