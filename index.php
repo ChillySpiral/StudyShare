572977efb9f5afb4bea355999fe80b8d704f496e
ChillySpiral <81975828+ChillySpiral@users.noreply.github.com>
2021-04-15 12:59:11 +0200

    Preise Optionen wurde hinzugefügt, Änderungen am Footer um den Responsive zu halten

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
 </head>
 
 <body style="background: #f7f7ff;">
+<div id="page-container">
     <nav class="navbar navbar-light navbar-expand-md sticky-top" id="my-navbar" style="background: #577399;border-radius: 0;border-bottom-right-radius: 7px;border-bottom-left-radius: 7px;">
         <div class="container-fluid"><a class="navbar-brand" id="header-links" href="#" style="font-size: 30px;">StudyShare</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse justify-content-end" id="navcol-1" style="color: rgb(255,255,255);">
@@ -21,12 +22,48 @@
             </div>
         </div>
     </nav>
+    <div id="Preise-Index" class="container text-center" style="text-align: center;">
+        <div class="row">
+            <div class="col-md-12">
+                <div style="border-bottom-width: 1px;border-bottom-style: solid;margin-right: 10%;margin-left: 10%;margin-bottom: 5%;">
+                    <h1>Preise</h1>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <div style="border-width: 1px;border-style: solid;border-radius: 45px;margin-right: 30px;margin-left: 30px;padding: 5px;margin-top: 5px;margin-bottom: 5px;">
+                    <h1>Option 1</h1>
+                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,</p>
+                    <h1>Info</h1>
+                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,</p><button class="btn btn-primary" type="button" style="background: #fe5f55;border-top-left-radius: 15px;border-top-right-radius: 15px;border-bottom-right-radius: 15px;border-bottom-left-radius: 15px;">Kaufen</button>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div style="border-width: 1px;border-style: solid;border-radius: 45px;margin-right: 30px;margin-left: 30px;padding: 5px;margin-bottom: 5px;margin-top: 5px;">
+                    <h1>Option 2</h1>
+                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,</p>
+                    <h1>Info</h1>
+                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,</p><button class="btn btn-primary" type="button" style="background: #fe5f55;border-radius: 15px;">Kaufen</button>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div style="border-width: 1px;border-style: solid;border-radius: 45px;margin-right: 30px;margin-left: 30px;padding: 5px;margin-top: 5px;margin-bottom: 5px;">
+                    <h1>Option 3</h1>
+                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,</p>
+                    <h1>Info</h1>
+                    <p style="text-align: center;">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,</p><button class="btn btn-primary" type="button" style="background: #fe5f55;border-radius: 15px;">Kaufen</button>
+                </div>
+            </div>
+        </div>
+    </div>
     <footer class="d-flex justify-content-end footer-basic" id="my-footer">
         <ul class="list-inline" style="background: #fe5f55;margin-top: 22px;margin-right: 30px;padding-right: 10px;padding-left: 10px;border-top-left-radius: 35px;border-top-right-radius: 35px;font-size: 24px;color: rgb(255,255,255);">
             <li class="list-inline-item"><a href="#">Impressum</a></li>
             <li class="list-inline-item"><a href="#">FAQ</a></li>
         </ul>
     </footer>
+</div>
     <script src="Javascript/jquery.min.js"></script>
     <script src="CSS/bootstrap/js/bootstrap.min.js"></script>
 </body>
