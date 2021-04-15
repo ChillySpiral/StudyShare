daf9d70e185ed669ed4078a7ea81f554b7b570ec
legarou <matidica@yahoo.com>
2021-04-15 13:15:13 +0200

    remove table

--- Components/signup.php
+++ Components/signup.php
@@ -13,31 +13,29 @@
         <img src="img_hello.jpg" alt="whoops" width="500" height="500">
         </div>
         <div>
-            <table>
-                <tr>
-                    <td><label for="vorname">Vorname: </label></td>
-                    <td><input type="text" id="vorname" name="vorname"></td>
-                </tr>
-                <tr>
-                    <td><label for="nachname">Nachname: </label></td>
-                    <td><input type="text" id="nachname" name="nachname"></td>
-                </tr>
-                <tr>
-                    <td><label for="email">E-Mail: </label></td>
-                    <td><input type="email" id="email" name="email"></td>
-                </tr>
-                <tr>
-                    <td><label for="passwort">Passwort: </label></td>
-                    <td><input type="password" id="password" name="password"></td>
-                </tr>
-                <tr>
-                    <td><label for="passwort2">Passwort widerholen: </label></td>
-                    <td><input type="password" id="password2" name="password2"></td>
-                </tr>    
-                <tr>
-                    <td><input type="submit" value="Registrieren"></td>
-                </tr>
-            </table>
+            <div>
+                <label for="vorname">Vorname: </label>
+                <input type="text" id="vorname" name="vorname">
+            </div>
+            <div>
+                <label for="nachname">Nachname: </label>
+                <input type="text" id="nachname" name="nachname">
+            </div>
+            <div>
+                <label for="email">E-Mail: </label>
+                <input type="email" id="email" name="email">
+            </div>
+            <div>
+                <label for="passwort">Passwort: </label>
+                <input type="password" id="password" name="password">
+                </div>
+            <div>
+                <label for="passwort2">Passwort widerholen: </label>
+                <input type="password" id="password2" name="password2">
+            </div>   
+            <div>
+                <input type="submit" value="Registrieren">
+            </div>
         </div>
     </div>
 </body>
