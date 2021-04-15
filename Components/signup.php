9db39a8c9afb6c5b3027de2060e43cd3d6d53cef
legarou <matidica@yahoo.com>
2021-04-15 12:35:34 +0200

    signup base

---
+++ Components/signup.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Studyshare</title>
+</head>
+
+<body>
+    <div style="background: #f7f7ff;">
+        <div>Bild</div>
+        <div>
+            <table>
+                <tr>
+                    <td><label for="vorname">Vorname: </label></td>
+                    <td><input type="text" id="vorname" name="vorname"></td>
+                </tr>
+                <tr>
+                    <td><label for="nachname">Nachname: </label></td>
+                    <td><input type="text" id="nachname" name="nachname"></td>
+                </tr>
+                <tr>
+                    <td><label for="email">E-Mail: </label></td>
+                    <td><input type="email" id="email" name="email"></td>
+                </tr>
+                <tr>
+                    <td><label for="passwort">Passwort: </label></td>
+                    <td><input type="password" id="password" name="password"></td>
+                </tr>
+                <tr>
+                    <td><label for="passwort2">Passwort widerholen: </label></td>
+                    <td><input type="password" id="password2" name="password2"></td>
+                </tr>    
+                <tr>
+                    <td><input type="submit" value="Registrieren"></td>
+                </tr>
+        </table>
+        </div>
+    </div>
+</body>
+
+</html>
