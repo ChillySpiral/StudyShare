e433da892b2b325ecdb6a97e9f8c8f207633f7f1
ChillySpiral <81975828+ChillySpiral@users.noreply.github.com>
2021-04-19 17:24:30 +0200

    Suchleiste hinzugefügt

--- index.php
+++ index.php
@@ -11,6 +11,9 @@
     <link rel="stylesheet" href="CSS/Login-Form-Clean.css">
     <link rel="stylesheet" href="CSS/Registration-Form-with-Photo.css">
     <link rel="stylesheet" href="CSS/styles.css">
+    <link rel="stylesheet" href="CSS/fonts/fontawesome-all.min.css">
+    <link rel="stylesheet" href="CSS/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="CSS/fonts/fontawesome5-overrides.min.css">
 </head>
 
 <body style="background: #f7f7ff;">
@@ -18,7 +21,19 @@
     <nav class="navbar navbar-light navbar-expand-md sticky-top" id="my-navbar" style="background: #577399;border-radius: 0;border-bottom-right-radius: 7px;border-bottom-left-radius: 7px;">
         <div class="container-fluid"><a class="navbar-brand" id="header-links" href="#" style="font-size: 30px;">StudyShare</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse justify-content-end" id="navcol-1" style="color: rgb(255,255,255);">
+            </form>
                 <ul class="navbar-nav">
+                <?php if(isset($_SESSION['user']))
+                { echo '
+                    <li class="nav-item" style="min-height: 40px;">
+                    <form action="" class="form-inline d-none d-sm-inline-block mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search" style="display: inline-block !important;">
+                        <div class="input-group">
+                            <input type="text" class="bg-light form-control border-0 small" placeholder="Suche Notizen, Zusammenfassungen & Mitschriften" style="border-top-left-radius: 15px;border-bottom-left-radius: 15px;" />
+                        <div class="input-group-append">
+                            <button type="submit" class="btn py-0" type="button" style="border-top-right-radius: 15px;border-bottom-right-radius: 15px;background: rgb(248,249,252);border-width: 1px;"><i class="fas fa-search" style="color: rgb(0,0,0);"></i></button></div>
+                        </div>
+                    </from>
+                    </li>';}?>
                     <li class="nav-item">
                     <a class="nav-link active" id="header-links" href="<?php if(isset($_SESSION['user'])){echo "#Logout";} else {echo "#Anmelden";}?>" style="padding: 8px;"><?php if(isset($_SESSION['user'])){echo "Logout";} else {echo "Anmelden";}?></a>
                     </li>
